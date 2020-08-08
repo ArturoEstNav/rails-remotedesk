17bf88bae94acab6b89d3ed07f5d16348da11756
Arturo Estrada <arturoestradanav@gmail.com>
2020-08-08 12:47:02 -0500

    user controller completed minus devise config

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :offers, only: [:index, :show]
 
-  resources :users, only: [:show, :edit, :uṕdate] do
+  resources :users, only: [:show, :edit] do
     resources :matches, only: [:index], shallow: true
   end
 end
