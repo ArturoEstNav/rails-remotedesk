5acf878143aaa30451b16754f38fe80b978c16b1
Arturo Estrada <arturoestradanav@gmail.com>
2020-08-08 13:39:19 -0500

    tag controller laid out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :matches
+  has_many :matches, dependent: :destroy
   has_many :offers, through: :matches
   # validates :first_name, :last_name, :gender, :residence_country, :postal_code, presence: true
 
