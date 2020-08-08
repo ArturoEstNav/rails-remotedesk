90cfbfb38d4bec6bb59278fe9bc7c92f1ca60e3f
Arturo Estrada <arturoestradanav@gmail.com>
2020-08-08 12:16:47 -0500

    fixed user validations and connected matches in users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :matches
-  validates :first_name, :last_name, :gender, :residence_country, :postal_code, presence: true
+  has_many :offers, through: :matches
+  # validates :first_name, :last_name, :gender, :residence_country, :postal_code, presence: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
