da3a35a005b0838a97f509287d85733e9cfbbb5b
Arturo Estrada <arturoestradanav@gmail.com>
2020-08-15 16:10:38 -0500

    Added test scraper to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,6 +43,11 @@ puts 'Create API offers'
 new_api_offer = ApiOffer.new
 new_api_offer.create_remotive_offers
 
+puts 'Create offers scraped from Indeed'
+
+indeed_offers = ScraperIndeed.new
+indeed_offers.create_indeed_offers
+
 puts 'Create new matches'
 User.all.each do |user|
   5.times do
