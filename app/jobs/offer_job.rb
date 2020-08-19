fa5d4de8030917900f2db02225c75d59f6d3e286
Bernardo Velasco <berveco@gmail.com>
2020-08-18 20:49:14 -0500

    fixed syncjob

--- app/jobs/offer_job.rb
+++ app/jobs/offer_job.rb
@@ -2,8 +2,9 @@ class OfferJob < ApplicationJob
   queue_as :default
 
   def perform
-    ApiOffer.create_remotive_offers
-    array_of_offers = ApiOffer.remotive_api_scrape
-    ApiOffer.update_offers_active_status(array_of_offers, 'remotive')
+    api_offer = ApiOffer.new
+    api_offer.create_remotive_offers
+    array_of_offers = api_offer.remotive_api_scrape
+    api_offer.update_offers_active_status(array_of_offers, 'remotive')
   end
 end
