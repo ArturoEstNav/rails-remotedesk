0ea6ca4c55f79ad7e70b2bb4e93c1387d859a544
Aaron Landeros <guiza92@hotmail.com>
2020-08-22 15:34:28 -0500

    Query filters in searchbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,13 @@ class PagesController < ApplicationController
 
   def home
     @tags = Tag.pluck(:name).sort
-    if params[:q].present?
+    if params[:q].present? && params[:job_type].present? && params[:location_type].present?
+      @offers = Offer.joins(:tags).where("tags.name = ? AND job_type = ? AND location_type = ?", params[:q], params[:job_type], params[:location_type])
+    elsif params[:q].present? && params[:location_type].present?
+      @offers = Offer.joins(:tags).where("tags.name = ? AND job_type = ?", params[:q], params[:location_type])
+    elsif params[:q].present? && params[:job_type].present?
+      @offers = Offer.joins(:tags).where("tags.name = ? AND job_type = ?", params[:q], params[:job_type])
+    elsif params[:q].present?
       @offers = Offer.joins(:tags).where('tags.name = ?', params[:q])
     end
     @params = params[:q]
