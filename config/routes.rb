c1d0db826ffa688f183b899e36ae9a9d60e9eeee
Bernardo Velasco <berveco@gmail.com>
2020-08-29 17:57:41 -0500

    user can create offers and fixed bug of profile name

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: 'pages#home'
+  get '/about', to: 'pages#about'
   get '/offers', to: 'pages#home'
-  resources :offers, only: [:show, :update]
+  resources :offers, only: [:show, :new, :create, :edit, :update, :destroy]
 
   # Sidekiq Web UI, only for admins.
   require "sidekiq/web"
