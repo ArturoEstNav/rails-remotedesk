24d94c021dc230c7908a53c824d1a8e217cf0091
Arturo Estrada <arturoestradanav@gmail.com>
2020-09-14 00:38:35 -0500

    new carousel implemented

--- app/javascript/packs/user_profile.js
+++ app/javascript/packs/user_profile.js
@@ -1,68 +1,42 @@
-// import Glide from '@glidejs/glide'
+const setCarousel = (jobList) => {
+  // add a class to 1st element in the array of carousel
+  let track = document.querySelector(`#${jobList} .custom-carousel-track`);
+  let slides = Array.from(track.children[0].children);
+  let prevButton = document.querySelector(`#${jobList} .custom-carousel-header .custom-controls`).children[0];
+  let nextButton = document.querySelector(`#${jobList} .custom-carousel-header .custom-controls`).children[1];
+  let slideSize = (slides[0].getBoundingClientRect().width + 24);
+  slides[0].className += " current";
 
-// let select = document.querySelector('.glide');
+  // align all the elements of the array in a linear fashion
+  let setSlidePosition = (slide, index) => {
+    slides[index].style.left = slideSize * index + "px"
+  }
 
-  // Create one per carousel: saved jobs, suggestions, created jobs
+  slides.forEach(setSlidePosition)
 
-  // console.log(saved);
+  const moveToSlide = (track, currentSlide, targetSlide) => {
+    // move to next offer
+    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    currentSlide.className = "custom-carousel-slide"
+    targetSlide.className += " current";
+  }
 
-  // let glide = new Glide('.glide', {
-  //   type: 'carousel',
-  //   perView: cardNumber
+  prevButton.addEventListener('click', e => {
+    let currentSlide = track.querySelector('.current');
+    let prevSlide = currentSlide.previousElementSibling;
 
-  // })
+    moveToSlide(track, currentSlide, prevSlide);
+  })
 
-  // let suggested = new Glide('.suggested', {
-  //   type: 'carousel',
-  //   perView: cardNumber
+  nextButton.addEventListener('click', e => {
+    let currentSlide = track.querySelector('.current');
+    let nextSlide = currentSlide.nextElementSibling;
 
-  // })
-
-  // let posted = new Glide('.posted', {
-  //   type: 'carousel',
-  //   perView: cardNumber
-
-  // })
-
-  // glide.mount();
-  // suggested.mount();
-  // posted.mount();
-
-// add a class to 1st element in the array of carousel
-let track = document.querySelector('#saved .custom-carousel-track');
-let slides = Array.from(track.children[0].children);
-let prevButton = document.querySelector('#saved .custom-carousel-header .custom-controls').children[0];
-let nextButton = document.querySelector('#saved .custom-carousel-header .custom-controls').children[1];
-let slideSize = (slides[0].getBoundingClientRect().width + 24);
-slides[0].className += " current";
-
-// align all the elements of the array in a linear fashion
-let setSlidePosition = (slide, index) => {
-  slides[index].style.left = slideSize * index + "px"
-}
-slides.forEach(setSlidePosition)
-// movr backward
-const moveToSlide = (track, currentSlide, targetSlide) => {
-  // move to next offer
-  track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
-  currentSlide.className = "custom-carousel-slide"
-  targetSlide.className += " current";
+    moveToSlide(track, currentSlide, nextSlide);
+    // Make sure that it cant get off limits
+  })
 }
 
-prevButton.addEventListener('click', e => {
-  let currentSlide = track.querySelector('.current');
-  let prevSlide = currentSlide.previousElementSibling;
-
-  moveToSlide(track, currentSlide, prevSlide);
-})
-// move forward
-
-
-nextButton.addEventListener('click', e => {
-  let currentSlide = track.querySelector('.current');
-  let nextSlide = currentSlide.nextElementSibling;
-
-  moveToSlide(track, currentSlide, nextSlide);
-})
-// using widths and position relative
-console.log(slideSize)
+setCarousel('saved');
+setCarousel('suggested');
+setCarousel('posted');
