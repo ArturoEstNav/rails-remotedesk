c41ba7dac5fbfc7280e832f39fcd28bbc471dccf
Arturo Estrada <arturoestradanav@gmail.com>
2020-11-22 14:18:11 -0600

    dashboard controls css structure set

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,40 +4,30 @@ class PagesController < ApplicationController
   def home
     @tags = Tag.pluck(:name).sort
     @new_offers = Offer.last(6)
+    # If remote or home office job and part and full time is selected
     if params[:q].present? && params[:job_type].present? && params[:location_type].present?
-      # Current working code
-        # @offers = Offer.joins(:tags).where("tags.name = ? AND job_type = ? AND location_type = ?", params[:q], params[:job_type], params[:location_type])
-      # Current working code
       if params[:sort] == "1"
         @offers = Offer.joins(:tags).where("tags.name = ? AND job_type = ? AND location_type = ?", params[:q], params[:job_type], params[:location_type]).order( posting_date: :desc)
       else
         @offers = Offer.joins(:tags).where("tags.name = ? AND job_type = ? AND location_type = ?", params[:q], params[:job_type], params[:location_type]).order( posting_date: :asc)
       end
-
+    # If remote or home office job is selected
     elsif params[:q].present? && params[:location_type].present?
-      # Current working code
-        # @offers = Offer.joins(:tags).where("tags.name = ? AND job_type = ?", params[:q], params[:location_type])
-      # Current working code
       if params[:sort] == "1"
         @offers = Offer.joins(:tags).where("tags.name = ? AND job_type = ?", params[:q], params[:location_type]).order( posting_date: :desc)
       else
         @offers = Offer.joins(:tags).where("tags.name = ? AND job_type = ?", params[:q], params[:location_type]).order( posting_date: :asc)
       end
-
+    # If full or partial time is selected
     elsif params[:q].present? && params[:job_type].present?
-      # Current working code
-        # @offers = Offer.joins(:tags).where("tags.name = ? AND job_type = ?", params[:q], params[:job_type])
-      # Current working code
       if params[:sort] == "1"
         @offers = Offer.joins(:tags).where("tags.name = ? AND job_type = ?", params[:q], params[:job_type]).order( posting_date: :desc)
       else
         @offers = Offer.joins(:tags).where("tags.name = ? AND job_type = ?", params[:q], params[:job_type]).order( posting_date: :asc)
       end
-
+    # If no options are selected
     elsif params[:q].present?
-      # Current working code
         @offers = Offer.joins(:tags).where('tags.name = ?', params[:q])
-      # Current working code
     end
     @query = params[:q]
     @job_type = params[:job_type]
@@ -47,6 +37,3 @@ class PagesController < ApplicationController
 
   def about; end
 end
-
-# .order( posting_date: :desc)
-# .order( posting_date: :asc)
