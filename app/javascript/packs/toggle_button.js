8c32aff53d413e0b1ac3de8ba1f0aa9d1110f9ea
Arturo Estrada <arturoestradanav@gmail.com>
2020-11-24 23:27:00 -0600

    javascript logic added, implementation still needed

--- app/javascript/packs/toggle_button.js
+++ app/javascript/packs/toggle_button.js
@@ -1,1 +1,21 @@
-// .custom-search-dashboard
+let dashboardToggle = document.getElementsByClassName("dashboard-toggle")[0],
+    hidableElements = document.getElementsByClassName("hidable-dashboard");
+
+let showOrHideDashboard = () => {
+  if(hidableElements[0].style.display === "none"){
+    // forEach
+    // bookListWrapper.style.display = "block";
+    hidableElements.forEach((element) => {
+      element.style.display === "block"
+    })
+  }
+  else{
+    // forEach
+    // bookListWrapper.style.display = "block";
+    hidableElements.forEach((element) => {
+      element.style.display === "none"
+    })
+  }
+}
+
+dashboardToggle.addEventListener("click", showOrHideDashboard);
