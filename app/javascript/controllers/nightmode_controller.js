0cec545f6d6047f5534c92a83f4e9594d19f1a67
Arturo Estrada <arturoestradanav@gmail.com>
2020-12-05 23:55:41 -0600

    laid out toggle logic

--- app/javascript/controllers/nightmode_controller.js
+++ app/javascript/controllers/nightmode_controller.js
@@ -1,9 +1,21 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
+  static targets = [ "page" ]
+
 
   connect() {
-    console.log("crash", this.element)
+    console.log(this.element.classList.contains("crash"))
+  }
+
+  adjustBackground(){
+    if(this.element.classList.contains("crash")) {
+     // this.inlineTarget.style.display = "none";
+      console.log("nay")
+
+    } else {
+      console.log("yay")
+    }
   }
 
 }
