c273e194b9b76567a39fb41190f0037e0654e97c
Arturo Estrada <arturoestradanav@gmail.com>
2020-12-06 00:23:36 -0600

    changed target and controller to body

--- app/javascript/controllers/nightmode_controller.js
+++ app/javascript/controllers/nightmode_controller.js
@@ -3,18 +3,11 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = [ "page" ]
 
-
-  connect() {
-    console.log(this.element.classList.contains("crash"))
-  }
-
   adjustBackground(){
-    if(this.element.classList.contains("crash")) {
-     // this.inlineTarget.style.display = "none";
-      console.log("nay")
-
+    if(this.element.classList.contains("dark-mode")) {
+      this.element.classList.remove("dark-mode");
     } else {
-      console.log("yay")
+      this.element.classList.add("dark-mode");
     }
   }
 
