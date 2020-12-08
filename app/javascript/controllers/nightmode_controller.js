bbab0c68d13b2f82c423d970127cab4ad1f72a94
Arturo Estrada <arturoestradanav@gmail.com>
2020-12-07 21:39:43 -0600

    added toggle target

--- app/javascript/controllers/nightmode_controller.js
+++ app/javascript/controllers/nightmode_controller.js
@@ -1,20 +1,27 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "page" ]
+  static targets = [ "page", "toggle" ]
 
   connect() {
-    if (localStorage.nightMode == 'true') this.element.classList.add("dark-mode");
+    if (localStorage.nightMode == 'true') {
+      this.element.classList.add("dark-mode");
+      this.toggleTarget.innerHTML = '<i class="fas fa-sun"></i>';
+    }
+
   }
 
   adjustBackground(){
     if(this.element.classList.contains("dark-mode") ) {
       this.element.classList.remove("dark-mode");
       localStorage.setItem('nightMode', 'false');
+      this.toggleTarget.innerHTML = '<i class="far fa-moon"></i>';
 
     } else {
       this.element.classList.add("dark-mode");
       localStorage.setItem('nightMode', 'true');
+      this.toggleTarget.innerHTML = '<i class="fas fa-sun"></i>';
+
     }
   }
 
