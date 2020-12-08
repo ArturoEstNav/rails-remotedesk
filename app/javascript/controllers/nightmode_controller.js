90944682f543d55b077691ac36e377143097611c
Arturo Estrada <arturoestradanav@gmail.com>
2020-12-07 21:04:45 -0600

    nightmode made persistent

--- app/javascript/controllers/nightmode_controller.js
+++ app/javascript/controllers/nightmode_controller.js
@@ -3,11 +3,18 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = [ "page" ]
 
+  connect() {
+    if (localStorage.nightMode == 'true') this.element.classList.add("dark-mode");
+  }
+
   adjustBackground(){
-    if(this.element.classList.contains("dark-mode")) {
+    if(this.element.classList.contains("dark-mode") ) {
       this.element.classList.remove("dark-mode");
+      localStorage.setItem('nightMode', 'false');
+
     } else {
       this.element.classList.add("dark-mode");
+      localStorage.setItem('nightMode', 'true');
     }
   }
 
